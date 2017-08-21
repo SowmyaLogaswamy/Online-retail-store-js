e4e6ff2f98c7e2f68ede36b8c5e1d11bdb60ef07
Guest User <Guest@epicodus-50.local>
2017-08-21 11:42:51 -0700

    Add welcome, about, marketplace components and a album model

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({  ///////Component Decorator
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+   ngOnInit() {
+   }
+
+}
